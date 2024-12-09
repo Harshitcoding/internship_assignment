52da958cc5f8b98042ea828e9ee78f7f38974ec4
Harshitcoding <khandelwalharshit96@gmail.com>
2024-12-09 21:14:53 +0530

    style

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -24,7 +24,7 @@ export default function Navbar() {
   }
 
   return (
-    <nav className="mt-10">
+    <nav className="mt-2">
       <div className="mx-auto  px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           {/* Logo */}
